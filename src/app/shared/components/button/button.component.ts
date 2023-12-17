1fef18d85b8d4d6f7d30839d8d2e7565584e1648
MACTEPwar <shebanic95@gmail.com>
2023-12-17 18:05:23 +0200

    fix

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -10,10 +10,14 @@ export class ButtonComponent implements OnInit {
   height = 48;
   pathShadow!: string;
   pathBackgroud!: string;
+  pathBorder!: string;
   offsetAngles = 26.1421;
 
   widthBg!: number;
   heightBg!: number;
+
+  widthBorder!: number;
+  heightBorder!: number;
 
   constructor(private elRef: ElementRef) {}
 
@@ -25,6 +29,7 @@ export class ButtonComponent implements OnInit {
 
     this.calcShadow();
     this.calcBackground();
+    this.calcBorder();
   }
 
   calcShadow(): void {
@@ -43,6 +48,86 @@ export class ButtonComponent implements OnInit {
     const cb3 = lrb - 7.071;
 
     this.pathShadow = `M19.0711 2.92893C20.9464 1.05357 23.49 0 26.1421 0H${w}C${ct1} 0 ${ct2} 1.05357 ${ct3} 2.92893L${lrt} 20.4645C${cr1} 22.4171 ${cr2} 25.5829 ${cr3} 27.5355L${lrb} 45.0711C${cb1} 46.9464 ${cb2} 48 ${cb3} 48H26.1421C23.49 48 20.9464 46.9464 19.0711 45.0711L1.53553 27.5355C-0.417086 25.5829 -0.417087 22.4171 1.53553 20.4645L19.0711 2.92893Z`;
+  }
+
+  calcBorder(): void {
+    const xOffsetAngles = 26.1421;
+    this.widthBorder = this.width;
+    this.heightBorder = this.height - 4 * 2;
+    const o = this.widthBorder - xOffsetAngles * 2;
+    const w = o + xOffsetAngles;
+    const ctrX1 = w + 2.453;
+    const ctrX2 = w + 4.806;
+    const ctrX3 = w + 6.541;
+    const lrtX = ctrX3 + 13.535;
+    const ccrX1 = lrtX + 1.66;
+    const ccrX2 = lrtX + 1.66;
+    const ccrX3 = lrtX + 0;
+    const lrbX = ccrX3 - 13.535;
+    const cbrX1 = lrbX - 1.735;
+    const cbrX2 = lrbX - 4.088;
+    const cbrX3 = lrbX - 6.541;
+
+    const hb = o / 2 + 6.3332;
+    const ccbrX1 = hb - 0.3786;
+    const ccbrX2 = hb - 0.747;
+    const ccbrX3 = hb - 1.05;
+    const lbrX = ccbrX3 - 2.5333;
+    const ccbbX1 = lbrX - 0.4444;
+    const ccbbX2 = lbrX - 1.0556;
+    const ccbbX3 = lbrX - 1.5;
+    const lblX = ccbbX3 - 2.5333;
+    const ccblX1 = lblX - 0.303;
+    const ccblX2 = lblX - 0.6714;
+    const ccblX3 = lblX - 1.05;
+
+    this.pathBorder = `
+    M19.6014 7.45926
+    C
+    21.3361 5.72455 
+    23.6889 4.75 
+    26.1421 4.75
+    H${w}
+    C
+    ${ctrX1} 4.75 
+    ${ctrX2} 5.72455 
+    ${ctrX3} 7.45926
+    L${lrtX} 20.9948
+    C
+    ${ccrX1} 22.6545 
+    ${ccrX2} 25.3455 
+    ${ccrX3} 27.0052
+    L${lrbX} 40.5407
+    C
+    ${cbrX1} 42.2755 
+    ${cbrX2} 43.25 
+    ${cbrX3} 43.25
+    H${hb}
+    C
+    ${ccbrX1} 43.25 
+    ${ccbrX2} 43.3728 
+    ${ccbrX3} 43.6
+    L${lbrX} 45.5
+    C
+    ${ccbbX1} 45.8333 
+    ${ccbbX2} 45.8333 
+    ${ccbbX3} 45.5
+    L${lblX} 43.6
+    C
+    ${ccblX1} 43.3728 
+    ${ccblX2} 43.25 
+    ${ccblX3} 43.25
+    H26.1421
+    C
+    23.6889 43.25 
+    21.3361 42.2754 
+    19.6014 40.5407
+    L6.06586 27.0052
+    C
+    4.40613 25.3455 
+    4.40614 22.6545 
+    6.06586 20.9948
+    L19.6014 7.45926Z`;
   }
 
   calcBackground(): void {
