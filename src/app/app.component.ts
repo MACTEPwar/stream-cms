4547ebfbdf008916ebf876318d7ee1bc57be0e18
MACTEPwar <shebanic95@gmail.com>
2024-06-22 15:47:01 +0300

    fix: vh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'stream-cms';
+
+  ngOnInit(): void {
+    this.setVh();
+    // alert(window.innerHeight)
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.setVh();
+  }
+
+  setVh(): void {
+    // Вычисляем высоту видимой области экрана
+    const vh = window.innerHeight;
+    // Устанавливаем значение CSS-переменной --vh
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 }
