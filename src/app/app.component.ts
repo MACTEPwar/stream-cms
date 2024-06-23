32d774a4a957e797aedec14d36f91a9bd9bd69a2
MACTEPwar <shebanic95@gmail.com>
2024-06-23 15:24:31 +0300

    A: Добавил меню в мобильный

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'stream-cms';
+  visibleNavs$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   ngOnInit(): void {
     this.setVh();
@@ -23,5 +25,9 @@ export class AppComponent {
     const vh = window.innerHeight;
     // Устанавливаем значение CSS-переменной --vh
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  changeVisibleNav(state: boolean): void {
+    this.visibleNavs$.next(state);
   }
 }
