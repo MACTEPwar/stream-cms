7a5ebe476ebd7a4598419149f21b8be29077c923
MACTEPwar <shebanic95@gmail.com>
2025-01-08 23:53:04 +0200

    U: обработка мне/не мне сообщение с сокета

--- src/app/core/app-services/player-socket.service.ts
+++ src/app/core/app-services/player-socket.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { ConfigService } from '@core';
 import { Observable, filter, map } from 'rxjs';
 import { Socket, io } from 'socket.io-client';
+import { AuthService } from 'src/app/core/app-services/auth.service';
 
 @Injectable()
 export class PlayerSocketService {
   private socket: Socket;
+  private authService: AuthService;
 
   constructor(private configService: ConfigService) {
+    this.authService = inject(AuthService);
     const url = this.configService.getValue<string>('config', 'apiURL')!;
     this.socket = io(url);
     this.onConnect().subscribe((res) => {
@@ -27,6 +30,13 @@ export class PlayerSocketService {
         observer.next(data);
       });
     }).pipe(
+      filter(
+        (f: any) =>
+          f.recipients === 'all' ||
+          (f.recipients as string[]).includes(
+            this.authService.currentUser$.getValue().id
+          )
+      ),
       filter((f: any) => f.code === commanCode),
       map((m) => m.data)
     );
